25c3b36b9087ee251932c651914b31e21d8fa700
scao0208 <1013222943@qq.com>
2024-06-21 16:22:28 +0800

    second version

---
+++ finetune.py
@@ -0,0 +1,162 @@
+import torch
+import torch.utils.data
+import numpy as np
+from tqdm import tqdm 
+from torchvision import transforms
+import lightning as L
+from lightning.pytorch.loggers import TensorBoardLogger
+from torchmetrics.detection.mean_ap import MeanAveragePrecision
+from train_part_mAP import LitAutoTrain
+from torchvision.transforms import PILToTensor
+from src.dataloader import collection
+from src.dataset import BDD100K, SampleBatches
+from lightning.pytorch.callbacks import ModelCheckpoint
+from torch.utils.tensorboard.writer import SummaryWriter
+
+from torch.utils.data import DataLoader
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
+
+
+
+data_mean = torch.tensor([0.485, 0.456, 0.406]) # need to find real one later
+data_std = torch.tensor([0.229, 0.224, 0.225]) # need to calculate 
+
+normalize = transforms.Normalize(
+            mean=list(data_mean),
+            std=list(data_std),
+        )
+
+transform = transforms.Compose([transforms.PILToTensor(), 
+                                 normalize])
+
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+print("==> Building model..")
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+checkpoint_path = "./lightning_logs/ckpt/3-bdd-fast-epoch=47-ratio=00-map_50=0.74.ckpt"
+checkpoint = torch.load(checkpoint_path, map_location=device)
+
+state_dict = checkpoint['state_dict']
+new_state_dict = dict()
+for k, v in state_dict.items():
+    if k.startswith("model"):
+        new_state_dict[k[6:]] = v
+    else:
+        new_state_dict[k] = v
+
+model = fasterrcnn_resnet50_fpn(weights=None, num_classes=11)
+
+model.load_state_dict(new_state_dict)
+model = model.to(device)
+
+ 
+def get_plabels2(model, samples):
+
+    sub5k_set = SampleBatches(root=train_path, feasible_list=samples, annFile="./bdd100k/labels/det_20/det_train_coco.json", transform=transforms.PILToTensor())
+    # sub5k_set, _ = torch.utils.data.random_split(sub5k_set, lengths=[10, len(sub5k_set) - 10])
+    sub5k = DataLoader(sub5k_set, batch_size=8, shuffle=False, collate_fn=collection, pin_memory=True)
+    
+    # entropy_indicators = list()
+    top1_scores = list()
+    model.eval()
+    with torch.no_grad():
+        for batch_idx, (inputs, targets, paths, indices) in enumerate(tqdm(sub5k, desc='getting labels', colour='yellow')):
+            # inputs, targets = inputs.to(device), targets.to(device)
+            inputs = [input.cuda() for input in inputs]
+            targets = [{k:v.cuda() for k, v in target.items()} for target in targets]
+            outputs = model(inputs)
+            map = MeanAveragePrecision(box_format='xywh', iou_type='bbox')
+
+            for i in range(len(inputs)):
+                list_target = [targets[i]]
+
+                output = [outputs[i]]
+                img_id = [indices[i]]
+
+                num_preds = len(output[0]["boxes"])
+                scores = output[0]["scores"]
+                # entropy = sum(- scores * torch.log(scores) - (1 - scores) * torch.log(1 - scores)).mean()
+                entropy = sum(- scores * torch.log(scores))
+                # entropy = sum(- scores * torch.log(scores)).mean()
+                
+                map.update(output, list_target)
+                map_50 = map.compute()["map_50"].item()
+                mar_large = map.compute()["mar_large"].item()
+            
+            top1_scores.append(map_50)
+            # entropy_indicators.append(entropy)
+    model.train()
+    idx = np.argsort(top1_scores)
+    samples = np.array(samples)
+    return samples[idx[:1000]]
+
+    
+if __name__ == "__main__":
+    
+    generator = torch.Generator()
+    generator.manual_seed(123)
+    CYCLE = 1
+    labeled = list()
+
+    train_path = "./bdd100k/images/100k/train"
+
+    trainset = SampleBatches(root=train_path, feasible_list=labeled, annFile="./bdd100k/labels/det_20/det_train_coco.json", transform=transforms.PILToTensor())
+    
+    logger = TensorBoardLogger("lightning_logs", name="v2", version="version_0")
+    writer = SummaryWriter(log_dir=logger.log_dir)
+
+    steps_validation = 0
+    fast_model = LitAutoTrain(model=model)
+
+    val_path = "./bdd100k/images/100k/val" # 1w imgs
+    valid_dataset = BDD100K(root=val_path, annFile="./bdd100k/labels/det_20/det_val_coco.json", transform=PILToTensor())
+    # valid_dataset, _ = torch.utils.data.random_split(valid_dataset, lengths=[10, len(valid_dataset) - 10])
+    valid_loader = DataLoader(dataset=valid_dataset, batch_size=4, shuffle=False, collate_fn=collection)
+    # logger = SummaryWriter(log_dir='./lightning_logs/')
+    for cycle in range(CYCLE):    
+        print('Cycle', cycle)
+
+        with open(f'./mAP50_id_v2/batch_0.txt', 'r') as f:
+            samples = f.readlines()
+            samples = [int(samples[i].strip("\n")) for i in range(len(samples))]
+            
+       
+        print('>> Getting previous checkpoint')
+        # checkpoint = glob.glob(f'./lightning_log/checkpoint_v2/cycle_{cycle-1}/4-sample-cycle={cycle-1:02d}-*.ckpt')
+        # # sampling
+        # checkpoint = torch.load(checkpoint[0], map_location=device)
+        # fast_model.load_state_dict(checkpoint['state_dict'])
+        # fast_model.to(device)
+        sample1k = get_plabels2(model, samples)
+            
+        # add 1k samples to labeled set
+        labeled.extend(sample1k)
+        print(f'>> Labeled length: {len(labeled)}')
+
+        trainset.feasible_list = labeled
+        train_loader = DataLoader(trainset, batch_size=8, shuffle=False, collate_fn=collection, pin_memory=True)
+        check_best=ModelCheckpoint(
+            save_top_k=1,
+            monitor="map_50",
+            mode="max",
+            dirpath=logger.log_dir + f"/checkpoint_v2/cycle_{cycle}",
+            filename = f"4-sample-cycle={cycle:02d}" + "-{epoch:02d}-{map_50:.2f}"
+        )
+
+        trainer = L.Trainer(
+            max_epochs=80,
+            callbacks=check_best,
+            logger=logger,
+            precision="16-mixed",
+            devices=[0],
+            log_every_n_steps=1,
+            gradient_clip_val=0.5)  
+ 
+
+        trainer.fit(model=fast_model, train_dataloaders=train_loader, val_dataloaders=valid_loader)
+        trainer.fit_loop.epoch_progress.current.completed = 0
+
+    writer.close()
+
+     
---
+++ train_part_entropy.py
@@ -0,0 +1,157 @@
+import torch
+import torch.utils.data
+import numpy as np
+from src.dataset import BDD100K
+from src.trainingModule import WarmupMultiStepLR
+from torchvision.transforms import PILToTensor
+
+from torchmetrics.detection.mean_ap import MeanAveragePrecision
+
+import lightning as L
+from lightning.pytorch.callbacks import ModelCheckpoint
+from typing import Any
+
+from torch.utils.data import DataLoader
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
+
+def collection(data):
+    images = []
+    targes = []
+    for sample in data:
+        images += [sample[0]]
+        targes += [sample[1]]
+    return images, targes
+
+
+# class LitAutoTrain(FasterRCNNLightning):
+#     def __init__(self, model: nn.Module, lr: float = 0.0001, iou_threshold: float = 0.5):
+#         super().__init__(model, lr, iou_threshold)
+
+class LitAutoTrain(L.LightningModule):
+    def __init__(self, model, lr: float = 0.01, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.model= model
+        self.lr = lr
+        # outputs
+        # self.training_step_outputs = []
+        # self.validation_step_outputs = []
+        # self.test_step_outputs = []
+        self.steps = 0
+        self.steps_validation = 0
+    
+    def forward(self, x, y):
+        return self.model(x, y)
+    
+    def training_step(self, batch, batch_idx):
+        # Batch
+        # x, y, x_name, y_name = batch  # tuple unpacking
+        if len(batch) == 2:
+            x, y = batch
+        elif len(batch) == 4:
+            x, y, _, _ = batch
+
+        loss_dict = self.model(x, y)
+        loss = sum(loss for loss in loss_dict.values())
+        # self.log_dict(loss_dict)
+        self.logger.log_metrics(loss_dict, step=self.steps)
+        self.steps += 1
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        # Batch
+        # x, y, x_name, y_name = batch
+        if len(batch) == 2:
+            x, y = batch
+        elif len(batch) == 4:
+            x, y, _, _ = batch
+        y_hat = self.model(x)
+
+        # calculate uncertainty 
+        pred_scores = y_hat[0]['scores']
+        val_entropy = sum(-pred_scores * torch.log(pred_scores))
+        if isinstance(val_entropy, int):
+            val_entropy = 0
+        else:
+            val_entropy = val_entropy.item()
+
+        # calculate MAP
+        map = MeanAveragePrecision(box_format="xywh", iou_type="bbox")
+        map.update(y_hat, y)
+        val_map = map.compute()
+        val_map.pop("classes") # corresponding to the cocoapi cocoeval.py line 378 + 379, np.float() -> np.float64() 
+        val_map50 = map.compute()["map_50"].item()
+
+        # calculate recall
+        # recall = map.compute()["mar_large"].item()
+
+        # combine the 3 above indicators in the validation accuracy
+        # val_acc = 0.4 * val_map50 + 0.4 * recall + 0.2 * np.exp(- 1e-2 * uncertainty) 
+        
+        self.log('val_entropy', val_entropy)
+        self.log_dict(val_map)
+        val_map['my_map_50'], val_map['val_entropy'],  = val_map50, val_entropy
+        self.logger.log_metrics(val_map, step=self.steps_validation)
+        self.steps_validation += 1
+        # print(f"steps: {self.steps_validation}")
+
+        return val_map
+        
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(
+            self.model.parameters(), lr=self.lr, momentum=0.9, weight_decay=0.0001
+        )
+        lr_scheduler = WarmupMultiStepLR(
+            optimizer, milestones=[240000, 320000]
+        )
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": lr_scheduler,
+            "monitor": "val_entropy"
+        }
+
+check_best=ModelCheckpoint(
+    save_top_k=1,
+    save_last=True,
+    monitor="val_entropy",
+    mode="min",
+    dirpath="./lightning_logs/entropy_ckpt",
+    filename="3-bdd-fast-{epoch:02d}-{ratio:02d}-{val_entropy:.2f}"
+)
+  
+    
+def train():
+
+    generator = torch.Generator()
+    generator.manual_seed(123)
+    train_path = "./bdd100k/images/100k/train" # 7w images
+    val_path = "./bdd100k/images/100k/val" # 1w imgs
+    
+    dataset = BDD100K(root=train_path, annFile="./bdd100k/labels/det_20/det_train_coco.json", transform=PILToTensor())
+    # divide train in to 20% labeled dataset and 80% unlabeled pool dataset
+    train_dataset, _ = torch.utils.data.random_split(dataset=dataset, lengths=[0.5, 0.5], generator=generator) 
+    # train_dataset, _ = torch.utils.data.random_split(dataset=dataset, lengths=[0.7, 0.3], generator=generator) 
+    valid_dataset = BDD100K(root=val_path, annFile="./bdd100k/labels/det_20/det_val_coco.json", transform=PILToTensor())
+
+
+    train_loader = DataLoader(dataset=train_dataset, batch_size=4, shuffle=False, collate_fn=collection)
+    valid_loader = DataLoader(dataset=valid_dataset, batch_size=4, shuffle=False, collate_fn=collection)
+    
+    
+    net = fasterrcnn_resnet50_fpn(weights=None, num_classes=11)
+
+    fast_model = LitAutoTrain(model=net)
+
+    trainer = L.Trainer(
+        max_epochs=70, 
+        accelerator="gpu", 
+        callbacks=check_best,
+        precision="16-mixed",
+        devices=[0,1,2,3], 
+        log_every_n_steps=1,
+        gradient_clip_val=0.5)
+    trainer.fit(model=fast_model, train_dataloaders=train_loader, val_dataloaders=valid_loader)
+
+if __name__ == "__main__":
+    train()
+
